923f7f5889b0abf94a6c77bdc585d6afb7607466
trextozyne <daleks1987@gmail.com>
2023-01-01 15:55:37 -0600

    learning the use of shared logic with jenkins

---
+++ var/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/use/bin/env groovy
+
+def call() {
+    echo 'bilding docker image...'
+    withCredentials([usernamePassword(credentialsId: '	Dockerhub-repo',passwordVariable: 'PASSWD', usernameVariable: 'USER')]) {
+        sh 'docker build -t trex1987/my-repo:jma-2.0 .'
+        sh 'echo #PASSWD | docker login -u USER --password-stdin'
+        sh 'docker push trex1987/my-repo:jma-2.0'
+    }
+}
